c3d62546e17def074497cffdc2edbdff183eb127
Adryd <me@adryd.com>
2019-10-07 22:23:52 -0400

    add prompt for brew installation to be finished

--- install/macos/brew.sh
+++ install/macos/brew.sh
@@ -2,6 +2,6 @@ cd ~
 curl -fsSL https://codeload.github.com/Homebrew/install/zip/master > brew.zip # raw.githubusercontent.com is blocked
 unzip brew.zip
 /usr/bin/ruby -e "$(cat ~/install-master/install)"
-wait
+read  -n 1 -p "press any key once brew is done" 
 rm -rf ./brew.zip
 rm -rf ./install-master
