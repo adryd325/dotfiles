6bd327dea2427d7d7ea5a56837d464cd743ab7ec
adryd <me@adryd.com>
2020-10-03 00:33:10 -0400

    More archiso tests

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 # detect archiso and run archiso scripts
-if lsblk -o name,mountpoint | grep 'arch_iso *\/$'; then
+if lsblk -o name,mountpoint | grep 'loop0 *\/run/archiso/sfs/airootfs$'; then
     ./opt/archinstall/archiso.sh
 fi
