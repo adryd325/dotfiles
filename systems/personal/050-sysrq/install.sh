a15800bb0f533841226d3c55d72673cb49ebecdf
adryd <me@adryd.com>
2021-05-05 22:54:54 -0400

    Updates.
    002-packages: Changed obs-studio and obs-v4l2-sink to obs-studio-browser
    050-sysrq: Allow for Alt+SysRq+K and Alt+SysRq+R for escaping a frozen X
    server

---
+++ systems/personal/050-sysrq/install.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+source $HOME/.adryd/constants.sh
+source $AR_DIR/lib/os.sh
+AR_MODULE="sysrq"
+
+if [ "$AR_OS" == "linux_archlinux" ]; then
+    [ ! -e "/etc/sysctl.d/10-sysrq.conf" ] \
+        && log info "Installing sysrq config" \
+        && sudo cp "$AR_DIR/systems/personal/050-sysrq/10-sysrq.conf" "/etc/sysctl.d/10-sysrq.conf"
+fi
