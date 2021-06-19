f366e9dba5279330b873ff6a4db5d25fbd00113e
adryd <me@adryd.com>
2021-06-19 18:44:29 -0400

    more fixes

--- systems/personal/003-packages/install.sh
+++ systems/personal/003-packages/install.sh
@@ -5,7 +5,8 @@ source $AR_DIR/lib/tmp.sh
 AR_MODULE="packages"
 
 if [ "$AR_OS" == "linux_arch" ]; then
-    yay -S "plymouth-git"
-    yay -S "gdm-plymouth-prime" "libgdm-plymouth-prime"
-    yay -S "gnome-shell-extensions"
+    yay -S --noconfirm --removemake "plymouth-git"
+    yay -S --noconfirm --removemake "libgdm-plymouth-prime"
+    yay -S --noconfirm --removemake "gdm-plymouth-prime"
+    yay -S --noconfirm --removemake "gnome-shell-extensions" "gnome-shell" "chrome-gnome-shell"
 fi
