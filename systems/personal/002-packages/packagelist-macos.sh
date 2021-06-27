63f74945fa913356a704ea8e80ea7603c649d379
adryd <me@adryd.com>
2021-06-26 23:31:07 -0400

    personal/packages: split package lists into sepperate files

---
+++ systems/personal/002-packages/packagelist-macos.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+source $HOME/.adryd/constants.sh
+brewTaps=("homebrew/cask-versions" "homebrew/cask-drivers" "homebrew/cask-fonts" "jeffreywildman/virt-manager")
+brewPackages=(
+    # GNU stuff and other CLI tools / libs
+    "bash"
+    "binutils"
+    "coreutils"
+    "curl"
+    "diffutils"
+    "ed"
+    "findutils"
+    "gawk"
+    "git"
+    "gnu-indent"
+    "gnu-sed"
+    "gnu-tar"
+    "gnu-which"
+    "grep"
+    "gzip"
+    "less"
+    "make"
+    "nano"
+    "ncurses"
+    "openssh"
+    "rsync"
+    "tmux"
+    "unzip"
+    "vim"
+    "watch"
+    "wget"
+    "zsh"
+    "zstd"
+    # Programming languages
+    "nodejs"
+    "python"
+    # CLI tools
+    "ffmpeg"
+    "htop"
+    "mas"
+    "youtube-dl" 
+    # Utilities
+    # apptrap always first
+    "apptrap"
+    "blockblock"
+    "keka"
+    "knockknock"
+    "lulu"
+    "netiquette"
+    "suspicious-package"
+    "virt-viewer"
+    # Base apps
+    "discord" 
+    "discord-canary"
+    "firefox"
+    "iterm2"
+    "visual-studio-code" 
+    # Extra
+    "affinity-designer-beta"
+    "tor-browser"
+    "font-cascadia-code"
+)
+masPackages=("407963104" "1037126344")
--- systems/personal/003-reflector/install.sh
+++ systems/personal/003-reflector/install.sh
@@ -7,5 +7,5 @@ AR_MODULE="reflector"
 if [ "$AR_OS" == "linux_arch" ] && pacman -Q reflector &> /dev/null; then
     log info "Generating mirror list"
     [ ! -e /etc/pacman.d/mirrorlist.arbak ] && sudo cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.arbak
-    reflector -f 10 -c CA -p https --ipv4 | sudo tee /etc/pacman.d/mirrorlist > /dev/null
+    reflector -f 10 -c CA,US -p http,https --ipv4 | sudo tee /etc/pacman.d/mirrorlist > /dev/null
 fi
