1d9c6e8a813fc30db01b1c5a70233c193b2ea7a3
adryd <me@adryd.com>
2021-07-13 01:46:13 -0400

    systems/ssh: apply system ssh config

---
+++ systems/personal/_install/30-ssh.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+[[ -z "$AR_DIR" ]] && echo "Please set AR_DIR in your environment" && exit 0; source $AR_DIR/constants.sh
+ar_os
+AR_MODULE="ssh"
+
+if [[ "$AR_OS" = "linux_arch" ]]; then
+    if [[ ! -d "$HOME/.ssh" ]]; then
+        mkdir "$HOME/.ssh"
+    fi
+    if [[ ! -f "$HOME/.ssh/id_ed25519" ]] > /dev/null; then
+        if ar_keyring && [[ -n "$AR_KEYRING" ]]; then
+            log info "Copying ssh configs and keys from keyring"
+            cp $AR_KEYRING/ssh/* "$HOME/.ssh"
+        fi
+    fi
+    if ! grep "# .ADRYD LOCK ($AR_MODULE)" /etc/ssh/sshd_config > /dev/null; then
+        log info "Applying sshd_config"
+        sudo tee -a /etc/ssh/sshd_config > /dev/null <<EOF
+# .ADRYD LOCK ($AR_MODULE) (this is to prevent the deploy script from infinitely appending this config to the end of the file)
+PermitRootLogin no
+GSSAPIAuthentication no
+PasswordAuthentication no
+PermitEmptyPasswords no
+AllowUsers $USER
+AuthenticationMethods publickey
+EOF
+    fi
+fi
