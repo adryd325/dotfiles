4d1f11cd842ea1389e009aca1833ff233a99a05d
adryd <me@adryd.com>
2021-07-13 00:47:24 -0400

    personal/gitconfig: set gitconfig

---
+++ systems/personal/_install/30-gitconfig.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+[[ -z "$AR_DIR" ]] && echo "Please set AR_DIR in your environment" && exit 0; source $AR_DIR/constants.sh
+ar_os
+AR_MODULE="gitconfig"
+
+if [[ "$AR_OS" = "linux_arch" ]]; then
+    if [[ ! -h "$HOME/.gitconfig" ]]; then
+        log info "Installing gitconfig"
+        ln -sf "$AR_DIR/systems/personal/$AR_MODULE/gitconfig" "$HOME/.gitconfig"
+    fi
+fi
