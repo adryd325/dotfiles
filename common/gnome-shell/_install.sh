e955e75e86309afd51d7cecbc0ed9ce5b6f764a8
adryd <me@adryd.com>
2022-02-27 01:41:21 -0500

    gnome-shell: add keybindings

--- common/gnome-shell/_install.sh
+++ common/gnome-shell/_install.sh
@@ -24,3 +24,19 @@ gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folder
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Creativity/ name 'Creativity'
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Utilities/ apps "['org.gnome.DiskUtility.desktop', 'org.gnome.eog.desktop', 'org.gnome.Evince.desktop', 'org.gnome.FileRoller.desktop', 'org.gnome.Extensions.desktop', 'org.gnome.Screenshot.desktop', 'org.gnome.tweaks.desktop', 'org.gnome.Totem.desktop', 'org.gnome.Calculator.desktop', 'pavucontrol.desktop', 'org.gnome.baobab.desktop', 'gnome-system-monitor.desktop', 'org.gnome.font-viewer.desktop', 'io.github.seadve.Kooha.desktop', 're.sonny.Junction.desktop', 'com.github.tchx84.Flatseal.desktop', 'piavpn.desktop', 'ca.desrt.dconf-editor.desktop', 'qt5ct.desktop']"
 gsettings set org.gnome.desktop.app-folders.folder:/org/gnome/desktop/app-folders/folders/Utilities/ name 'Utilities'
+
+# Setting keybinds
+gsettings set org.gnome.settings-daemon.plugins.media-keys custom-keybindings "['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/pkill/']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/pkill/ name 'Force Quit (Xorg)'
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/pkill/ command "bash -c \"xprop -root | awk '/_NET_ACTIVE_WINDOW\(WINDOW\)/{print \$NF}' | xargs xprop -id | awk '/_NET_WM_PID\(CARDINAL\)/{print \$NF}' | xargs kill\""
+gsettings set org.gnome.settings-daemon.plugins.media-keys.custom-keybinding:/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/pkill/ binding "<Shift><Control>q"
+
+gsettings set org.gnome.settings-daemon.plugins.media-keys next "['<Shift><Super>Right']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys play "['<Shift><Super>Down']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys previous "['<Shift><Super>Left']"
+gsettings set org.gnome.settings-daemon.plugins.media-keys stop "['<Shift><Super>Up']"
+
+gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-right "[]"
+gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-down "[]"
+gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-left "[]"
+gsettings set org.gnome.desktop.wm.keybindings move-to-monitor-up "[]"
