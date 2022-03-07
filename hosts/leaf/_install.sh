56955f9742464a20fe7535fb9302fdf102333044
adryd <me@adryd.com>
2022-03-06 20:54:25 -0500

    add more to leaf install script

--- hosts/leaf/_install.sh
+++ hosts/leaf/_install.sh
@@ -2,8 +2,27 @@
 cd "$(dirname "$0")" || exit $?
 source ../../lib/log.sh
 [[ "${USER}" = "root" ]] && log error "Do not run as root" && exit 1
+#!/usr/bin/env bash
+cd "$(dirname "$0")" || exit $?
+
+source ../../lib/log.sh
+source ../../lib/os.sh
 
 ../../common/bash/_install.sh
-../../oses/linux/discord/_install.sh "stable" "canary"
+../../common/update-global-installation.sh
+../../common/bash/_install.sh globalInstall
+
+# Update everything
+sudo dnf upgrade
+
+# ./install-packages.sh || exit $?
+../../common/sysctl/_install.sh
+../../common/xdg-user-dirs/_install.sh
+../../common/mimeapps/_install.sh
+../../common/hide-internal-apps/hide-internal-apps.sh
+../../common/gnome-shell/_install.sh
+../../common/gnome-terminal/_install.sh
+../../common/vscode/_install.sh
+../../common/discord/_install.sh "stable" "canary"
 ../../common/heckheating/_install.sh "stable" "canary"
-../../oses/linux/discord/wmclass-fix/_install.sh "stable" "canary"
+../../common/discord/wmclass-fix.sh "stable" "canary"
