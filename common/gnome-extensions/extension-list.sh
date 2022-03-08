fe9e190c04490021f79376741c4025a12f3770c4
adryd <me@adryd.com>
2022-03-08 00:02:53 -0500

    common/gnome-extensions: WIP extension install script

---
+++ common/gnome-extensions/extension-list.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+extensions=(
+    'notifications_to_file@fawtytoo'
+    'hotedge@jonathan.jdoda.ca'
+    'lock-screen-message@advendradeswanta.gitlab.com'
+    'AlphabeticalAppGrid@stuarthayhurst'
+    'tiling-assistant@leleat-on-github'
+    'gtktitlebar@velitasali.github.io'
+    'x11gestures@joseexposito.github.io'
+    'unite@hardpixel.eu'
+    'just-perfection-desktop@just-perfection'
+    'compiz-windows-effect@hermes83.github.com'
+    'desktop-cube@schneegans.github.com'
+    'appindicatorsupport@rgcjonas.gmail.com'
+    'nightthemeswitcher@romainvigier.fr'
+    'blur-my-shell@aunetx'
+)
+
+export extensions
