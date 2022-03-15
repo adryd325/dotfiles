1cff0d47e9eb77560d38d183310c12f4346edd01
adryd <me@adryd.com>
2022-03-15 02:01:28 -0400

    common/bash: alias youtube-dl to yt-dlp when available

--- common/bash/bash_aliases.sh
+++ common/bash/bash_aliases.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 [[ -x $(command -v xdg-open) ]] && alias open="xdg-open"
+[[ -x $(command -v yt-dlp) ]] && alias youtube-dl='yt-dlp'
 alias ls='ls --color=auto'
 alias ll='ls --color=auto -alF'
 alias la='ls --color=auto -A'
@@ -7,9 +8,5 @@ alias l='ls --color=auto -CF'
 alias grep='grep --color=auto'
 alias fgrep='fgrep --color=auto'
 alias egrep='egrep --color=auto'
-
-alias rm='rm -i'
-alias cp='cp -i'
-alias mv='mv -i'
 
 alias cd..="cd .."
