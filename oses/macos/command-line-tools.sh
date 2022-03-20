79e9d583834a0ca8bc478b11e554b774048d645d
adryd <me@adryd.com>
2022-03-19 22:37:41 -0400

    Steal command-line-tools script from brew

--- oses/macos/command-line-tools.sh
+++ oses/macos/command-line-tools.sh
@@ -3,7 +3,27 @@ cd "$(dirname "$0")" || exit $?
 source ../../lib/log.sh
 AR_MODULE="command-line-tools"
 
+# from https://github.com/Homebrew/install/blob/master/install.sh
 if ! xcode-select -p &> /dev/null; then
-    log info "Installing command line tools"
-    xcode-select --install
+    log info "Searching online for the Command Line Tools"
+    # This temporary file prompts the 'softwareupdate' utility to list the Command Line Tools
+    clt_placeholder="/tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress"
+    sudo touch "${clt_placeholder}"
+
+    clt_label=$(
+        /usr/sbin/softwareupdate -l |
+        grep -B 1 -E 'Command Line Tools' |
+        awk -F'*' '/^ *\\*/ {print \$2}' |
+        sed -e 's/^ *Label: //' -e 's/^ *//' |
+        sort -V |
+        tail -n1
+    )
+
+    if [[ -n "${clt_label}" ]]
+    then
+        ohai "Installing ${clt_label}"
+        sudo /usr/sbin/softwareupdate -i "${clt_label}"
+        sudo xcode-select --switch "/Library/Developer/CommandLineTools"
+    fi
+    sudo rm -f "${clt_placeholder}"
 fi
