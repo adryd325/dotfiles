eb60b24d0a7611ab826d2b515b29c636710f13ed
adryd <me@adryd.com>
2022-03-24 02:30:31 -0400

    common/bash: installation uses cp over ln

--- common/bash/_install.sh
+++ common/bash/_install.sh
@@ -5,7 +5,7 @@ source ../../lib/realpath.sh
 
 if [[ -d "/opt/adryd-dotfiles" ]] && [[ "$1" == "globalInstall" ]]; then
     [[ -e "/root/.bashrc" ]] && [[ ! -e "/root/.bashrc.orig" ]] && cp "/root/bashrc" "/root/.bashrc.orig"
-    sudo cp -f /opt/adryd-dotfiles/common/bash/bashrc "/root/.bashrc"
+    sudo cp -f /opt/adryd-dotfiles/common/bash/bashrc "/root/.bashrc" 2> /dev/null
 
     mapfile -t IDs <<< "$(cut -d ":" -f3 /etc/passwd)"
     mapfile -t homeFolders <<< "$(cut -d ":" -f6 /etc/passwd)"
@@ -16,11 +16,10 @@ if [[ -d "/opt/adryd-dotfiles" ]] && [[ "$1" == "globalInstall" ]]; then
         if [[ ${IDs[${i}]} -gt 999 ]] && [[ ${IDs[${i}]} -lt 2000 ]]; then
             home="${homeFolders[${i}]}"
             [[ -e "${home}/.bashrc" ]] && [[ ! -e "${home}/.bashrc.orig" ]] && cp "${home}/.bashrc" "${home}/.bashrc.orig"
-            # no -f to not override local installation
-            sudo cp /opt/adryd-dotfiles/common/bash/bashrc "${home}/.bashrc" 2> /dev/null
+            sudo cp -f /opt/adryd-dotfiles/common/bash/bashrc "${home}/.bashrc" 2> /dev/null
         fi
     done
 else
     [[ -e "${HOME}/.bashrc" ]] && [[ ! -e "${HOME}/.bashrc.orig" ]] && cp "${HOME}/.bashrc" "${HOME}/.bashrc.orig"
-    ln -sf "$(realpath "./bashrc")" "${HOME}/.bashrc"
+    cp -f "$(realpath "./bashrc")" "${HOME}/.bashrc" 2> /dev/null
 fi
