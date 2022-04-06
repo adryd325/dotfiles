f880e0cf57c7abc9eff8c8c14b4916ecc314032e
adryd <me@adryd.com>
2022-04-05 23:59:07 -0400

    oses/archlinux/pacman: add fcgu repo installer

---
+++ oses/archlinux/pacman/fcgu.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+cd "$(dirname "$0")" || exit $?
+source ../../../lib/log.sh
+AR_MODULE="fcgu"
+
+[[ ! -e /etc/pacman.conf.orig ]] && sudo cp /etc/pacman.conf /etc/pacman.conf.orig
+lockStr="## adryd-dotfiles-lock (fcgu)"
+if ! grep "^${lockStr}" /etc/pacman.conf > /dev/null; then
+    sudo pacman-key --keyserver hkps://keys.openpgp.org --recv-keys 6E58E886A8E07538A2485FAED6A4F386B4881229
+    sudo pacman-key --lsign-key 6E58E886A8E07538A2485FAED6A4F386B4881229
+    sudo pacman -U https://vmi394248.contaboserver.net/fcgu/fcgu-mirrorlist-1-1-any.pkg.tar.zst --noconfirm
+    orig="[core]\nInclude = /etc/pacman.d/mirrorlist"
+    sudo sed -i "/\[core\]/{N;s:\[core\]\nInclude = /etc/pacman.d/mirrorlist:${orig}\n\n${lockStr}\n[fcgu]\nInclude = /etc/pacman.d/fcgu-mirrorlist:}" /etc/pacman.conf
+    log info "Enable fcgu repo"
+fi
