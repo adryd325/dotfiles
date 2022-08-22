b568d67fb25ab30c5e9034bbeab0ba3852f49ddd
adryd <me@adryd.com>
2022-08-21 22:16:50 -0400

    oses/archlinux/pacman/fcgu: update mirrorlist package

--- oses/archlinux/pacman/fcgu.sh
+++ oses/archlinux/pacman/fcgu.sh
@@ -8,8 +8,10 @@ lockStr="## adryd-dotfiles-lock (fcgu)"
 if ! grep "^${lockStr}" /etc/pacman.conf > /dev/null; then
     sudo pacman-key --keyserver hkps://keys.openpgp.org --recv-keys 6E58E886A8E07538A2485FAED6A4F386B4881229
     sudo pacman-key --lsign-key 6E58E886A8E07538A2485FAED6A4F386B4881229
-    sudo pacman -U https://vmi394248.contaboserver.net/fcgu/fcgu-mirrorlist-1-1-any.pkg.tar.zst --noconfirm
+    sudo pacman -U https://vmi394248.contaboserver.net/fcgu/fcgu-mirrorlist-2-1-any.pkg.tar.zst --noconfirm
     orig="[core]\nInclude = /etc/pacman.d/mirrorlist"
     sudo sed -i "/\[core\]/{N;s:\[core\]\nInclude = /etc/pacman.d/mirrorlist:${orig}\n\n${lockStr}\n[fcgu]\nInclude = /etc/pacman.d/fcgu-mirrorlist:}" /etc/pacman.conf
     log info "Enable fcgu repo"
+else
+    sudo pacman -U https://vmi394248.contaboserver.net/fcgu/fcgu-mirrorlist-2-1-any.pkg.tar.zst --noconfirm
 fi
