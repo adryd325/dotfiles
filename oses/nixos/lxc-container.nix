c9ed1bd2ecaf51dad786fb5a46e088e8ca0f8c33
adryd <me@adryd.com>
2023-08-01 01:29:07 -0400

    meowers

--- oses/nixos/lxc-container.nix
+++ oses/nixos/lxc-container.nix
@@ -5,9 +5,10 @@
 
   networking = {
     domain = "in.adryd.com";
-    enableIPv6 = false;
+    enableIPv6 = true;
     defaultGateway = "10.142.0.1";
-    nameservers = [ "10.142.0.1" ];
+    defaultGateway6 = "2a0f:85c1:212:ffff::1";
+    nameservers = [ "10.142.0.1", "2a0f:85c1:212:ffff::1" ];
     dhcpcd.enable = false;
   };
 }
