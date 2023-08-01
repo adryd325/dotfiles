3947cece9e59d91a6907a37ff3233285097d070b
adryd <me@adryd.com>
2023-08-01 01:33:31 -0400

    no ipv6 dns pls

--- oses/nixos/lxc-container.nix
+++ oses/nixos/lxc-container.nix
@@ -8,7 +8,7 @@
     enableIPv6 = true;
     defaultGateway = "10.142.0.1";
     defaultGateway6 = "2a0f:85c1:212:ffff::1";
-    nameservers = [ "10.142.0.1" "2a0f:85c1:212:ffff::1" ];
+    nameservers = [ "10.142.0.1" ];
     dhcpcd.enable = false;
   };
 }
