18201d8ea31162e966b5a18e4f31744fd5822111
adryd <me@adryd.com>
2023-08-01 01:44:57 -0400

    meowy3

--- oses/nixos/lxc-container.nix
+++ oses/nixos/lxc-container.nix
@@ -5,7 +5,6 @@
 
   networking = {
     domain = "in.adryd.com";
-    enableIPv6 = true;
     defaultGateway = "10.142.0.1";
     defaultGateway6 = "2a0f:85c1:212:ffff::1";
     nameservers = [ "10.142.0.1" ];
