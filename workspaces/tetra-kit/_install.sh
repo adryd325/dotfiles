971e295d323429368fa9c0b56ee450a25b66e6d4
adryd <me@adryd.com>
2024-01-15 19:04:17 -0500

    mode set for _install

--- workspaces/tetra-kit/_install.sh
+++ workspaces/tetra-kit/_install.sh
@@ -1,4 +1,7 @@
 #!/usr/bin/env bash
+cd "$(dirname "$0")" || exit $?
+source ../../lib/log.sh
+AR_MODULE="tetra-kit"
 
 if ! [[ -d "${HOME}/_/tetra-kit" ]]; then
     log info "Installing tetra-kit workspace"
